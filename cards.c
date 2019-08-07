1c9f10ca161dcda05c144c053a2f8c4ebe10ae3d
SeiichiN <billie175@gmail.com>
2019-08-07 17:41:51 +0900

    次はポインタ

--- cards.c
+++ cards.c
@@ -1,7 +1,6 @@
 /*
  * cards.c
- * p19
- * コードマグネット
+ * p35
  * カードの値を評価するプログラム
  */
 #include <stdio.h>
@@ -9,27 +8,36 @@
 int main()
 {
   char card_name[3];
-  puts("カード名を入力してください");
-  scanf("%2s", card_name);
-  int val = 0;
-  switch (card_name[0]) {
-	case 'K':
-	case 'Q':
-	case 'J':
-	  val = 10;
-	  break;
-	case 'A':
-	  val = 11;
-	  break;
-	default:
-	  val = atoi(card_name);
-  }
-  /* 値が 3 から 6 かどうかを調べる */
-  if (val >= 3 && val <= 6)
-	puts("カウントが増えます ");
-  /* それ以外の場合には、カードが 10、J、Q、または K かどうかを調べる */
-  else if ( val == 10 )
-	puts("カウントが減ります ");
-
+  int count = 0;
+  do {
+	puts("カード名を入力してください");
+	scanf("%2s", card_name);
+	int val = 0;
+	switch (card_name[0]) {
+	  case 'K':
+	  case 'Q':
+	  case 'J':
+		val = 10;
+		break;
+	  case 'A':
+		val = 11;
+		break;
+	  case 'X':
+		continue;
+	  default:
+		val = atoi(card_name);
+		if (val < 1 || val > 10) {
+		  printf("数が範囲外です。");
+		  continue;
+		}
+	}
+	/* 値が 3 から 6 かどうかを調べる */
+	if (val >= 3 && val <= 6)
+	  count++;
+	/* それ以外の場合には、カードが 10、J、Q、または K かどうかを調べる */
+	else if ( val == 10 )
+	  count--;
+	printf("現在のカウント：%i\n", count);
+  } while (card_name[0] != 'X');
   return 0;
 }
---
+++ p34.c
@@ -0,0 +1,18 @@
+/*
+ * p.34.c
+ */
+#include <stdio.h>
+
+int main()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5) {
+	x = x + 1;
+	y = y + x;
+	printf("%i%i ", x, y);
+	x = x + 1;
+  }
+  return 0;
+}
+
