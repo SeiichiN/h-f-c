a17dd1f9eeba4e83a7d3d145c8d0998e86f451cf
SeiichiN <billie175@gmail.com>
2019-08-26 01:18:28 +0900

    open_url

---
+++ chap10/rss/openurl.c
@@ -0,0 +1,52 @@
+/*
+ * openurl.c
+ * p446
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+void open_url(char *url)
+{
+    char launch[255];
+    /*
+    sprintf(launch, "cmd /c start %s", url);
+    system(launch);
+    */
+    /*
+    sprintf(launch, "x-www-browser '%s' &", url);
+    system(launch);
+    */
+    /*
+    sprintf(launch, "open '%s'", url);
+    system(launch);
+    */
+
+    sprintf(launch, "'%s' &", url);
+    printf("%s\n", launch);
+
+    pid_t pid = fork();
+    if (pid == -1)
+        error("プロセスをフォークできません");
+    if (!pid) {
+        if (execl("/usr/bin/x-www-browser", launch,  NULL) == -1) {
+            fprintf(stderr, "スクリプトを実行できません: %s\n", strerror(errno));
+        }
+    }
+}
+
+int main()
+{
+    char *url = "http://www.billies-works.com/";
+    open_url(url);
+
+    return (0);
+}
