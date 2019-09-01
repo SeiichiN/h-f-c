a54e0b411a5b11053b8bd98d2b8e59fe319d820a
SeiichiN <billie175@gmail.com>
2019-09-02 07:29:17 +0900

    chap12 end.

---
+++ chap12/p516B.c
@@ -0,0 +1,54 @@
+/*
+ * p516B.c
+ * p516
+ * ビールマグネット
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+int beers = 2000000;
+
+
+pthread_mutex_t beers_lock = PTHREAD_MUTEX_INITIALIZER;
+void* drink_lots(void *a)
+{
+    int i;
+    for (i = 0; i < 100000; i++) {
+        pthread_mutex_lock(&beers_lock);
+        beers = beers - 1;
+        pthread_mutex_unlock(&beers_lock);
+    }
+    printf("beers = %i\n", beers);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t threads[20];
+    int t;
+    printf("壁にはビールが %i 本 \n%i 本のビール\n", beers, beers);
+    for (t = 0; t < 20; t++) {
+        if (pthread_create ( &threads[t], NULL, drink_lots, NULL ) == -1)
+            error("スレッド threads[t] を作成できません");
+    }
+    void* result;
+    for (t = 0; t < 20; t++) {
+        if (pthread_join (threads[t], &result) == -1)
+            error("スレッド threads[t] をジョインできません");
+    }
+    printf("現在、壁にはビールが %i 本あります\n", beers);
+    return 0;
+}
+
+        
